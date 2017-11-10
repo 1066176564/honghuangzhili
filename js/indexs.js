13f2894c43cc4a367ff8fc1ed6d8f6f39e71d714
chongjibo <1066176564@qq.com>
2017-11-10 18:48:21 +0800

    程聚波s

--- js/indexs.js
+++ js/indexs.js
@@ -13,19 +13,32 @@ define(['jquery'],function($){
 		$("#div1").fadeIn(200).siblings().hide()
 		
 		$("li:eq(0)").click(function(){
+			smoothscroll();
 			$("#div1").fadeIn(200).siblings().hide()
 		})
 
 		$("li:eq(1)").click(function(){
+			smoothscroll();
 			$("#div2").fadeIn(200).siblings().hide()
 		})
 
 		$("li:eq(2)").click(function(){
+			smoothscroll();
+
 			$("#div3").fadeIn(200).siblings().hide()
 		})
 
 		$("li:eq(3)").click(function(){
+			smoothscroll();
 			$("#div4").fadeIn(200).siblings().hide()
 		})
+
+		function smoothscroll(){
+	    var currentScroll = document.documentElement.scrollTop || document.body.scrollTop;
+	    if (currentScroll > 0) {
+	         window.requestAnimationFrame(smoothscroll);
+	         window.scrollTo (0,currentScroll - (currentScroll));
+	   	 }
+		}
 	
 })
--- js/list.js
+++ js/list.js
@@ -75,12 +75,16 @@ define(["ajax"],function(ajax){
   	}
   //回到顶部
 	$(".img").click(smoothscroll);
+
 		function smoothscroll(){
 	    var currentScroll = document.documentElement.scrollTop || document.body.scrollTop;
 	    if (currentScroll > 0) {
 	         window.requestAnimationFrame(smoothscroll);
 	         window.scrollTo (0,currentScroll - (currentScroll/5));
-	    }
-	}
+	   	 }
+		}
+
+
+
 	
 })
